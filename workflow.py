9f860f8df9ce23c3cdab164707e82fce1d6ebf8b
Joaquin-Developer <joaquin.p.olivera@gmail.com>
2023-06-02 18:17:27 -0300

    prueba modified-files 14

--- workflow.py
+++ workflow.py
@@ -1,16 +1,19 @@
 import sys
+import os
 
 
 def main():
-    modified_files = sys.argv[1]
+    modified_files = sys.argv[1:]
 
-    print(modified_files)
 
     for file in modified_files:
         print(file)
     
     print("Hi")
     print(len(modified_files))
+
+    output = os.getenv("GITHUB_OUTPUT")
+    print(output)
 
 if __name__ == "__main__":
     main()
