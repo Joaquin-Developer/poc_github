c0a531a8369e0d64105b3b512f576937cb4bce34
Joaquin-Developer <joaquin.p.olivera@gmail.com>
2023-06-02 17:44:41 -0300

    prueba modified-files

--- workflow.py
+++ workflow.py
@@ -4,10 +4,11 @@ import sys
 def main():
     author = sys.argv[1]
     message = sys.argv[2]
-    data = sys.argv[3]
+    modified_files = sys.argv[3]
+
+
     print(author, message)
-    print(type(data))
-    print(vars(data))
+    print(modified_files)
 
 
 if __name__ == "__main__":
