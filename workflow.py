38583f51be7b7634e19150da3afdcd2dc40cbd67
Joaquin-Developer <joaquin.p.olivera@gmail.com>
2023-06-02 17:39:49 -0300

    prueba

--- workflow.py
+++ workflow.py
@@ -4,7 +4,10 @@ import sys
 def main():
     author = sys.argv[1]
     message = sys.argv[2]
+    data = sys.argv[3]
     print(author, message)
+    print(type(data))
+    print(data)
 
 
 if __name__ == "__main__":
