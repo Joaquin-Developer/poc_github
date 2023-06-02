bb37b11841e79613b3b67812f2c39827ae6fd54e
Joaquin-Developer <joaquin.p.olivera@gmail.com>
2023-06-02 17:48:29 -0300

    prueba modified-files 2

--- workflow.py
+++ workflow.py
@@ -2,13 +2,7 @@ import sys
 
 
 def main():
-    author = sys.argv[1]
-    message = sys.argv[2]
-    modified_files = sys.argv[3]
-
-    modified_files = sys.argv[3:]
-
-    print(author, message)
+    modified_files = sys.argv[1:]
 
     for file in modified_files:
         print(file)
